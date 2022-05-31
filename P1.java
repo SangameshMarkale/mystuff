edb838433a3527218070828bdbe4219166887e5f
SangameshMarkale <96579055+SangameshMarkale@users.noreply.github.com>
2022-05-31 13:07:19 +0530

    First Program

---
+++ P1.java
@@ -0,0 +1,8 @@
+package Demo;
+import java.util.*;
+//Write a Java program to convert a hash set to an array.
+public class P1 {
+    public static void main(String[] args) {
+        HashSet<String> h=new HashSet<>()
+    }
+}
